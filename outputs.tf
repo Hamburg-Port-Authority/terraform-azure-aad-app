133244acaa48b1fe533203d3eb7e15ca580c4553
Artem Lajko <artem.lajko@hpa.hamburg.de>
2024-01-05 12:35:39 +0100

    add new output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,14 @@ output "object_id" {
   value       = azuread_application.main.object_id
   description = "The Applications object ID."
 }
+output "azuread_service_principal_id" {
+  value       = azuread_service_principal.main.id
+  description = "The Application ID (also called Client ID)."
+}
+
+output "azuread_service_principal_object_id" {
+  value       = azuread_service_principal.main.object_id
+  description = "The Applications object ID."
+}
+
+
