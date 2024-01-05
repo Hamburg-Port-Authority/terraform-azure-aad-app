c374189edb40d60634d12695e56f1acf01e82ac2
Artem Lajko <artem.lajko@hpa.hamburg.de>
2024-01-05 12:37:10 +0100

    fix message

--- outputs.tf
+++ outputs.tf
@@ -9,12 +9,12 @@ output "object_id" {
 }
 output "azuread_service_principal_id" {
   value       = azuread_service_principal.main.id
-  description = "The Application ID (also called Client ID)."
+  description = "The Service Principal ID."
 }
 
 output "azuread_service_principal_object_id" {
   value       = azuread_service_principal.main.object_id
-  description = "The Applications object ID."
+  description = "The Service Principal Object ID."
 }
 
 
